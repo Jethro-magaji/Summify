26d48ae26add90b98983d75afd23ebc437ae2c0c
jethro-magaji <jemagaji38@gmail.com>
2021-07-03 00:55:42 +0100

    🚧 Work in Progress:

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
-var express = require('express');
-var router = express.Router();
-var request = require("request");
+// create an express app
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const fs = require('fs');
+const crypto = require('crypto');
+
+
+// use the express-static middleware
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(express.static("public"));
